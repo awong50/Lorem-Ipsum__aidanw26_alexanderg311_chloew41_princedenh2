dddbf5cfb2987972c5e0f586895bd492fec0ae2f
Princeden Hom <phom50@stuy.edu>
2025-05-12 18:07:34 -0400

    began routes?

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,19 @@
+import ReactDom from "react-dom/client"
+import {createBrowserRouter, Routes, Route, createRoutesFromElements, RouterProvider} from "react-router-dom"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './pages/App.tsx'
+import Home from './pages/Home.tsx'
+import Header from "./pages/Header.tsx"
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Header />}>
+      <Route index element={<Home />} />
 
+    </Route>
+  )
+)
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
