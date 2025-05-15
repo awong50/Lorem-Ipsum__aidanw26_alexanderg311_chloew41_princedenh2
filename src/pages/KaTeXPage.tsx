7c1a07dab878d2d74fa8c4500bd64ea26fdac2cb
chloepwong <cwong50@stuy.edu>
2025-05-15 12:13:36 -0400

    katex user input

--- src/pages/KaTeXPage.tsx
+++ src/pages/KaTeXPage.tsx
@@ -13,15 +13,22 @@ const LatexPage = () => {
       };
 
   const containerRef = useRef<HTMLDivElement>(null);
+
   const latexSource = `
     \\zeta(s) = \\sum_{n \\geq 1} \\frac{1}{n^s} \\quad \\quad \\forall{s > 1}
+    \\left(\\frac{\\int_0^\\infty e^{-x^2} \\, \\mathrm{d}x}{\\frac{\\sqrt\\pi}{2}}\\right) = 1
+    \\zeta(3) = \\frac{5}{2} \\sum_{n=1}^\\infty \\frac{(-1)^{n-1}}{n^3 \\binom{2n}{n}}
   `;
-  //\\left(\\frac{\\int_0^\\infty e^{-x^2} \\, \\mathrm{d}x}{\\frac{\\sqrt\\pi}{2}}\\right) = 1
-  //\\zeta(3) = \\frac{5}{2} \\sum_{n=1}^\\infty \\frac{(-1)^{n-1}}{n^3 \\binom{2n}{n}}
+
+  const sampleEquation1 = '\\zeta(s) = \\sum_{n \\geq 1} \\frac{1}{n^s} \\quad \\quad \\forall{s > 1}'
+  const sampleEquation2 = '\\left(\\frac{\\int_0^\\infty e^{-x^2} \\, \\mathrm{d}x}{\\frac{\\sqrt\\pi}{2}}\\right) = 1'
+  const sampleEquation3 = '\\zeta(3) = \\frac{5}{2} \\sum_{n=1}^\\infty \\frac{(-1)^{n-1}}{n^3 \\binom{2n}{n}}'
 
   useEffect(() => {
-    if (containerRef.current) {
+    if (containerRef.current && equation) {
       containerRef.current.innerHTML = ''; // Clear previous content
+      
+      /*
       latexSource.split('\n').forEach((line) => {
         if (line.trim()) {
           const equationElement = document.createElement('div');
@@ -30,10 +37,16 @@ const LatexPage = () => {
             displayMode: true, // Render in display mode
           });
           containerRef.current?.appendChild(equationElement);
-        }
+      */
+
+      const equationElement = document.createElement('div');
+      katex.render(equation, equationElement, {
+        throwOnError: false,
+        displayMode: true, // Render in display mode
       });
+      containerRef.current?.appendChild(equationElement);
     }
-  }, []);
+  }, [equation]);
 
   return (
     <div>
@@ -42,9 +55,12 @@ const LatexPage = () => {
         <input type="text" value={equation}
           onChange={handleInputChange} 
           placeholder="Enter equation" />
-        <p>Equation: {equation}</p>
+        {/* <p>Input: {equation}</p> */}
       </div>
       <div ref={containerRef}></div>
+      <p><b>Sample Equation 1:</b> {sampleEquation1}</p>
+      <p><b>Sample Equation 2:</b> {sampleEquation2}</p>
+      <p><b>Sample Equation 3:</b> {sampleEquation3}</p>
     </div>
   );
 };
