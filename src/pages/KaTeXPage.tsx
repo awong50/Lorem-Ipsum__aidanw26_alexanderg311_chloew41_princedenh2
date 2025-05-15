1710adbeb48cacdd49e3053ca085476698676154
Chloe Wong <cwong50@stuy.edu>
2025-05-15 10:56:19 -0400

    user inputs

--- src/pages/KaTeXPage.tsx
+++ src/pages/KaTeXPage.tsx
@@ -1,14 +1,23 @@
 import { useEffect, useRef } from 'react';
+import { useState } from 'react';
 import katex from 'katex';
 import 'katex/dist/katex.min.css';
 
 const LatexPage = () => {
+
+  const [equation, setEquation] = useState<string>('');
+
+  const handleInputChange = 
+      (e: React.ChangeEvent<HTMLInputElement>) => {
+          setEquation(e.target.value);
+      };
+
   const containerRef = useRef<HTMLDivElement>(null);
   const latexSource = `
     \\zeta(s) = \\sum_{n \\geq 1} \\frac{1}{n^s} \\quad \\quad \\forall{s > 1}
-    \\left(\\frac{\\int_0^\\infty e^{-x^2} \\, \\mathrm{d}x}{\\frac{\\sqrt\\pi}{2}}\\right) = 1
-    \\zeta(3) = \\frac{5}{2} \\sum_{n=1}^\\infty \\frac{(-1)^{n-1}}{n^3 \\binom{2n}{n}}
   `;
+  //\\left(\\frac{\\int_0^\\infty e^{-x^2} \\, \\mathrm{d}x}{\\frac{\\sqrt\\pi}{2}}\\right) = 1
+  //\\zeta(3) = \\frac{5}{2} \\sum_{n=1}^\\infty \\frac{(-1)^{n-1}}{n^3 \\binom{2n}{n}}
 
   useEffect(() => {
     if (containerRef.current) {
@@ -29,6 +38,12 @@ const LatexPage = () => {
   return (
     <div>
       <h1>Compiling LaTeX with KaTeX</h1>
+      <div>
+        <input type="text" value={equation}
+          onChange={handleInputChange} 
+          placeholder="Enter equation" />
+        <p>Equation: {equation}</p>
+      </div>
       <div ref={containerRef}></div>
     </div>
   );
