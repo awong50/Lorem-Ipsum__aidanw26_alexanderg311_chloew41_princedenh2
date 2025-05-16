8f610eb677eb3e877c101983a726eb49374afc76
Princeden Hom <phom50@stuy.edu>
2025-05-16 10:33:21 -0400

    user queries?

--- server/db/user-query.ts
+++ server/db/user-query.ts
@@ -1,10 +1,23 @@
 import mongoose from 'mongoose';
 import User from '../models/User';
+import { error } from 'console';
 
-const user_query = async (str:
-     username) => {
-const t = User;
-const users = await t.findOne({name: username});
+const userQuery = async  (username: string) => {
+	try {
+		const users = await User.findOne({name: username});
+	return users;
+	} catch (e) {
+	console.log(e);
+	}
 
 
-}
+};
+userQuery('Test Man').then((value) => {
+		console.log(value);
+
+})
+.catch((error) => {
+	console.error('aa');
+
+});
+// export default userQuery;
