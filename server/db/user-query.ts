dc433769ceb3294d6a49c9473e7cc4855e0baf9d
Aidan Wong <awong50@stuy.edu>
2025-05-16 10:38:57 -0400

    Created Test Function

--- server/db/user-query.ts
+++ server/db/user-query.ts
@@ -1,23 +1,18 @@
 import mongoose from 'mongoose';
 import User from '../models/User';
-import { error } from 'console';
+import connectDB from './mongoose';
 
-const userQuery = async  (username: string) => {
-	try {
-		const users = await User.findOne({name: username});
-	return users;
-	} catch (e) {
-	console.log(e);
-	}
-
-
+const user_query = async (username: string) => {
+  const user = await User.findOne({ name: username });
+  return user;
 };
-userQuery('Test Man').then((value) => {
-		console.log(value);
 
-})
-.catch((error) => {
-	console.error('aa');
+// Test function
+const testUserQuery = async () => {
+  await connectDB(); // Connect to the database
+  const result = await user_query('Test Man'); // Replace 'testuser' with the username you want to query
+  console.log('Query Result:', result);
+  mongoose.connection.close(); // Close the database connection
+};
 
-});
-// export default userQuery;
+testUserQuery();
