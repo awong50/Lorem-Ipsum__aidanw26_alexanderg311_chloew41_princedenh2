9dcabe0d3178c64fd163db91f41f88e78b1ec247
Aidan <aidanwonglam@gmail.com>
2025-05-19 20:58:30 -0400

    Got started on login functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Register from './pages/Register';
 import About from './pages/About';
 import LatexPage from './pages/KaTeXPage';
 import TypingPage from './pages/Typing';
+import Login from './pages/Login';
 import Protected from './pages/Protected';
 
 function App() {
@@ -12,15 +13,16 @@ function App() {
       <nav>
         <Link to="/">Home</Link> |{' '}
         <Link to="/register">Registration Page</Link> |{' '}
+        <Link to="/login">Login</Link> |{' '}
         <Link to="/about">About</Link> |{' '}
         <Link to="/latex">View LaTeX Page</Link> |{' '}
         <Link to="/typing">View Typing Page</Link> |{' '}
         <Link to="/protected">Protected</Link>
       </nav>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/latex" element={<LatexPage />} />
         <Route path="/typing" element={<TypingPage />} />
